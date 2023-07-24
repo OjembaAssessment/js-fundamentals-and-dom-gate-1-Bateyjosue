aae1574c4bccd3db40bf4576e3d1396a51ab3f5d
Bateyjosue <josuebatey19@gmail.com>
2023-07-24 12:40:49 +0200

    Add function to split into paragraph

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,3 +1,25 @@
 const myParagraph = document.querySelector('#myParagraph');
 
-console.log(myParagraph.innerText);
+let para = myParagraph.innerText;
+
+function countRepeatedWords(paragraph) {
+    let words = paragraph.split(" ");
+    let objWord = {};
+    for (let i = 0; i < words.length; i++) {
+      let currentWordCount = objWord[words[i]];
+      let count = currentWordCount ? currentWordCount : 0;
+      objWord[words[i]] = count + 1;
+    }
+    return objWord;
+  }
+  commonWord = countRepeatedWords(para)
+  
+  arrayWord =  Object.entries(commonWord)
+  
+  let obj = {}
+  let arr =[]
+  arrayWord.forEach(el => {
+      obj[el[0]] = el[1]
+      arr.push({el[0]:el[1]})
+  })
+  
