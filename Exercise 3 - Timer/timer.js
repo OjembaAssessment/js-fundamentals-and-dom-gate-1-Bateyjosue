7a41eb2ea294cedb2658a74b6be24bbe58bd88c3
Bateyjosue <josuebatey19@gmail.com>
2023-07-24 12:13:26 +0200

    Reset timer on user click

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -4,6 +4,8 @@ const substrat = document.querySelector('#subtract')
 const action = document.querySelector('.action')
 const timer = document.querySelector('.timer')
 
+const reset = document.querySelector('#reset')
+
 
 add.addEventListener('click', ()=>{
     let value  = timer.textContent
@@ -15,4 +17,8 @@ substrat.addEventListener('click', ()=>{
     if (+value) {
         timer.textContent = parseInt(value, 10) - 1
     }
+});
+
+reset.addEventListener('click', ()=>{
+        timer.textContent = 0
 });
